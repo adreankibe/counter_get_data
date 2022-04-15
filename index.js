b47ec14be6e80f38091fa2a054e15dd4efc4f861
Evans Mburu <njoguem9@gmail.com>
2022-04-15 16:19:33 +0300

    v1

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-require('body-parser-xml')(bodyParser);
-app.use(
-    bodyParser.xml({
-      limit: '150MB', // Reject payload bigger than 1 MB
-      xmlParseOptions: {
-        normalize: true, // Trim whitespace inside text nodes
-        normalizeTags: true, // Transform tags to lowercase
-        explicitArray: false, // Only put nodes in array if >1
-      },
-    }),
-  );
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended:true}))
 
-app.post('/api/post_url',(req,res,body)=>{
+app.use(bodyParser.text({type:"*/*"}));
+
+app.post('/api/post_url',(req,res)=>{
     console.log(req.body);
+ 
     res.status(200).end();
 })
-app.post('/api/heart_url',(req,res,body)=>{
-    console.log(req.body);
-    res.status(200).end();
-})
-app.post('/',(req,res)=>{
-    console.log(req.body)
-})
+// app.post('/api/heart_url',(req,res,body)=>{
+//     console.log(req.body);
+//     res.status(200).end();
+// })
+// app.post('/',(req,res)=>{
+//     console.log(req.body)
+// })
 app.listen(7050,()=>{
     console.log('running on port 7050')
 })
