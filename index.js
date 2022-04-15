84de779e3b2cf67d55cacfca9373a3c169018fad
Evans Mburu <njoguem9@gmail.com>
2022-04-15 15:46:12 +0300

    v1

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+require('body-parser-xml')(bodyParser);
+app.use(
+    bodyParser.xml({
+      limit: '1MB', // Reject payload bigger than 1 MB
+      xmlParseOptions: {
+        normalize: true, // Trim whitespace inside text nodes
+        normalizeTags: true, // Transform tags to lowercase
+        explicitArray: false, // Only put nodes in array if >1
+      },
+    }),
+  );
+
+app.post('/api/post_url',(req,res,body)=>{
+    console.log(req.body);
+    res.status(200).end();
+})
+app.post('/api/heart_url',(req,res,body)=>{
+    console.log(req.body);
+    res.status(200).end();
+})
+// //app.post('/',(req,res)=>{
+//     console.log(req.body)
+// })
+app.listen(7050,()=>{
+    console.log('running on port 7050')
+})
