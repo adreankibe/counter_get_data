abec4f37c2488a932689ced3c9d1867f5938268e
Evans Mburu <njoguem9@gmail.com>
2022-04-17 20:08:57 +0300

    v1

--- index.js
+++ index.js
@@ -2,41 +2,42 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 var parser = require('xml2json');
-app.use(bodyParser.text({type:"*/*"}));
+app.use(bodyParser.text({ type: "*/*" }));
 
 const heart_url = require('./save_data/heart_url');
 const post_url = require('./save_data/post_url');
-
-app.post('/api/PostUrl',(req,res)=>{
+const save_event = require('./save_data/events');
+app.post('/api/PostUrl', (req, res) => {
     let xml = req.body;
     let json = parser.toJson(xml)
     json = JSON.parse(json)
     console.log(json.RTMetrics.ReportData.Report.Date)
     console.log(json.RTMetrics.ReportData.Report.Object.Count)
     let data = {
-        device_id:json.RTMetrics.DeviceId,
-        date:json.RTMetrics.ReportData.Report.Date,
-        count:json.RTMetrics.ReportData.Report.Object.Count
+        device_id: json.RTMetrics.DeviceId,
+        date: json.RTMetrics.ReportData.Report.Date,
+        count: json.RTMetrics.ReportData.Report.Object.Count
     }
 
     // console.log(data)
-post_url(data)
-  
+    save_event(data)
+    post_url(data)
+
 })
-app.post('/api/HeartUrl',(req,res)=>{
+app.post('/api/HeartUrl', (req, res) => {
     let xml = req.body;
     let json = parser.toJson(xml)
     json = JSON.parse(json)
     let data = {
-        device_id:json.Diags.DeviceId,
-        properties:json.Diags.Properties
+        device_id: json.Diags.DeviceId,
+        properties: json.Diags.Properties
     }
     //console.log(data)
     heart_url(data)
-    
+
 })
 
 
-app.listen(8070,()=>{
+app.listen(8070, () => {
     console.log('running on port 8070')
 })
--- save_data/events.js
+++ save_data/events.js
@@ -14,25 +14,9 @@ const update_event = (data) => {
     const Building = db.collection('buildings')
 
     const device_id = data.device_id;
-    const date = data.date;
     const count = data.count;
 
-    let currentdate = new Date(date);
-    var oneJan = new Date(currentdate.getFullYear(), 0, 1);
-    var numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000));
-    var week_no = Math.ceil((currentdate.getDay() + 1 + numberOfDays) / 7);
-    const year = currentdate.getFullYear();
-    const nm = currentdate.getMonth() + 1
-    const day = ("0" + currentdate.getDate()).slice(-2);
-    const day_ = currentdate.getDate()
-    const month = ("0" + (currentdate.getMonth() + 1)).slice(-2);
-    const today = year + "-" + month + "-" + day
-    const year_month = year + "-" + month
-    const start_date = year_month + "-01"
-    const hour = addZero(currentdate.getHours())
-    const minute = addZero(currentdate.getMinutes())
-    const time = hour + ":" + minute;
-
+ 
     let total_entry = 0
     if (Array.isArray(count) === true) {
         var lastItem = count.pop();
@@ -41,6 +25,7 @@ const update_event = (data) => {
     else if (Array.isArray(count) === false) {
         total_entry += parseInt(count.Enters)
     }
+
 
     Outlet.findOne({ device_id: device_id }, (err, outlet) => {
         if (outlet) {
@@ -58,9 +43,6 @@ const update_event = (data) => {
             })
         }
     })
-
-
-
 
 
 
