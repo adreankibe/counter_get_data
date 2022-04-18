35a7a55abf31fa9874c0ab70cc774747e925e918
Evans Mburu <njoguem9@gmail.com>
2022-04-18 21:43:03 +0300

    v1

--- save_data/post_url.js
+++ save_data/post_url.js
@@ -16,7 +16,7 @@ const post_url = (data) => {
     const date = data.date;
     const count = data.count;
    
-    let currentdate = new Date(date);
+    let currentdate = new Date();
     var oneJan = new Date(currentdate.getFullYear(), 0, 1);
     var numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000));
     var week_no = Math.ceil((currentdate.getDay() + 1 + numberOfDays) / 7);
